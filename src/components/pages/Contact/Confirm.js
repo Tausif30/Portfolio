d2d09e98a4f5afec1de48a8f8d08d3ecc077bf67
Tausif30 <tausif3003@gmail.com>
2024-06-16 02:53:00 +0600

    Changed the check circle in confirmation page.

--- src/components/pages/Contact/Confirm.js
+++ src/components/pages/Contact/Confirm.js
@@ -1,10 +1,9 @@
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import check from "./check-circle.svg"
 import './Confirm.css'
 const Confirm = () => {
   return (
     <div className="container confirmed-booking">
-      <FontAwesomeIcon icon={faCircleCheck} size="3x" style={{color: "#4D403A",}} />
+      <img src={check} alt='check'></img>
       <h2>I have received your mail.</h2>
       <p className='confirmation'>I will contact you soon.</p>
     </div>
